e03a0287fe43746b8def3ca6bb0ebd5af213fd94
oussamacherair <oussamacherair@gmail.com>
2025-05-29 01:58:25 +0200

    work

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
 
       <body>
         <Header/>
-      <main className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <main className={`${geistSans.variable} ${geistMono.variable} `}>
         {children} 
         </main>
         <Footer/>
